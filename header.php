e54a63c684a04162068f6d8cf2df2595a0fb600d
Paul Fleury <loporifle@gmail.com>
2019-01-05 09:43:05 +0100

    mieux

--- header.php
+++ header.php
@@ -35,6 +35,31 @@
     <div id="containerWrapper" class="wrapper">
         
         <header id="header" class="blog-header">
+            
+            <section id="blogHeaderTop" class="blog-header__top">
+                <div class="container">
+                    <div class="top-contact">
+                        <span class="strong uppercase">Acérola Carrière</span> &nbsp; Paris • Neuilly-Plaisance • Montévrain</a></span>
+                        <span><?php echo get_build_icon_path('phone.svg') ?> 01 41 79 17 93</span>
+                        <span><?php echo get_build_icon_path('mail.svg') ?> <a href="https://www.acerolacarriere.fr/contact/">contact</a></span>
+                    </div>
+                    <div class="top-social">
+                        <a href="https://www.facebook.com/acerolacarriere" class="" target="_blank">
+                            <?php echo get_build_icon_path('facebook.svg') ?>
+                        </a>
+                        <a href="https://twitter.com/AcerolaCarriere" class="" target="_blank">
+                            <?php echo get_build_icon_path('twitter.svg') ?>
+                        </a>
+                        <a href="https://www.linkedin.com/profile/view?id=222313042" class="picto-social picto-social--sm" target="_blank">
+                            <?php echo get_build_icon_path('linkedin.svg') ?>
+                        </a>
+                        <a href="http://www.viadeo.com/profile/002l8hk33l3bnvr" class="" target="_blank">
+                            <?php echo get_build_icon_path('viadeo.svg') ?>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
             <section id="blogBanner" class="blog-header__banner">
                 <div class="container">
                     <div id="siteTitle" class="site-title">
