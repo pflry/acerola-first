86de4e074273e6a34c5c70182deb24ea9e817966
Paul Fleury <loporifle@gmail.com>
2019-01-25 15:57:21 +0100

    bloggy

--- entry-meta.php
+++ entry-meta.php
@@ -10,13 +10,6 @@
  */
 
 ?><section class="entry-meta">
-    <?php 
-        $authorID = get_the_author_meta( 'ID' );
-        $authorAvatar = get_avatar( $authorID, '35' );
-    ?>
-    <div class="entry-meta__author">
-        <?php echo $authorAvatar; ?><span class="author vcard"><?php the_author_meta('first_name'); ?>&nbsp;<?php the_author_meta('last_name'); ?>, <?php the_author_meta('description'); ?></span>
-    </div>
     <div class="content">
         <div class="entry-meta__date">
             <?php echo get_build_icon_path('calendar.svg') ?> Publié le <span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
@@ -26,5 +19,7 @@
             <?php the_tags( $etiquette, $etiquette ); ?>
         </div>
     </div>
-    
+    <div class="entry-meta__author">
+        <?php echo get_build_icon_path('user.svg') ?>Par&nbsp;<span class="author vcard"><?php the_author_meta('first_name'); ?>&nbsp;<?php the_author_meta('last_name'); ?></span><span class="bullet">&bullet;</span><?php the_author_meta('description'); ?>
+    </div>
 </section>
